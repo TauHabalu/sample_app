03ead0550dc08f8351196844aeb5a9013c8f34ce
Tau Habalu <sergeysoloshenko@gmail.com>
2016-06-07 08:06:46 +0000

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def new
@@ -29,7 +30,7 @@ class UsersController < ApplicationController
   def edit
     
   end
-  
+  св
   def update
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
@@ -52,12 +53,12 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
     
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
+    #def signed_in_user
+    #  unless signed_in?
+    #    store_location
+    #    redirect_to signin_url, notice: "Please sign in."
+    #  end
+    #end
     
     def correct_user
       @user = User.find(params[:id])
